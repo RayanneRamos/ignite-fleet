f13478ff341d88b896239d698e9b7cfe8176ef45
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-15 17:02:58 -0300

    obtendo dados do perfil

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,7 +4,8 @@ import { Button } from "../../components/Button";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
 import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from "@env";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -25,6 +26,24 @@ export function SignIn() {
       }
     });
   }
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      if (response.authentication?.idToken) {
+        fetch(
+          `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${response.authentication.idToken}`
+        )
+          .then((response) => response.json())
+          .then(console.log);
+      } else {
+        Alert.alert(
+          "Entrar",
+          "Não foi possível conectar-se a sua conta Google."
+        );
+        setIsAuthenticating(false);
+      }
+    }
+  }, [response]);
 
   return (
     <Container source={backgroundImage}>
