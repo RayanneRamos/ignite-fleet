bf95e9c9e922dd97b46070fba118bc52dedd2ae3
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-15 16:38:40 -0300

    obtendo o token de usuário

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,13 +1,40 @@
 import { Container, Slogan, Title } from "./styles";
 import backgroundImage from "../../assets/background.png";
 import { Button } from "../../components/Button";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from "@env";
+import { useState } from "react";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [_, response, googleSignIn] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ["profile", "email"],
+  });
+
+  function handleGoogleSignIn() {
+    setIsAuthenticating(true);
+
+    googleSignIn().then((response) => {
+      if (response.type !== "success") {
+        setIsAuthenticating(false);
+      }
+    });
+  }
+
   return (
     <Container source={backgroundImage}>
       <Title>Ignite Fleet</Title>
       <Slogan>Gestão de uso de veículos</Slogan>
-      <Button title="Entrar com Google" />
+      <Button
+        title="Entrar com Google"
+        onPress={handleGoogleSignIn}
+        isLoading={isAuthenticating}
+      />
     </Container>
   );
 }
