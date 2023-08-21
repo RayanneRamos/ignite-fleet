e3383e47665f1d18d96c097449088145a479eba9
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-21 15:48:42 -0300

    imagem do usuário

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,11 +1,15 @@
 import { TouchableOpacity } from "react-native";
-import { Container, Greeting, Message, Name } from "./styles";
+import { Container, Greeting, Message, Name, Picture } from "./styles";
 import { Power } from "phosphor-react-native";
 import theme from "../../theme";
 
 export function HomeHeader() {
   return (
     <Container>
+      <Picture
+        source={{ uri: "https://github.com/RayanneRamos.png" }}
+        placeholder="L184i9offjof00ayf7ay~qjtj[jt"
+      />
       <Greeting>
         <Message>Olá</Message>
         <Name>Rayanne</Name>
