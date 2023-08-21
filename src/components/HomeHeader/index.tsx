541c851dcbdd22f765e53e20753383fc3e5bf086
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-21 15:50:09 -0300

    carregando dados do usuário

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -2,17 +2,20 @@ import { TouchableOpacity } from "react-native";
 import { Container, Greeting, Message, Name, Picture } from "./styles";
 import { Power } from "phosphor-react-native";
 import theme from "../../theme";
+import { useUser } from "@realm/react";
 
 export function HomeHeader() {
+  const user = useUser();
+
   return (
     <Container>
       <Picture
-        source={{ uri: "https://github.com/RayanneRamos.png" }}
+        source={{ uri: user.profile.pictureUrl }}
         placeholder="L184i9offjof00ayf7ay~qjtj[jt"
       />
       <Greeting>
         <Message>Olá</Message>
-        <Name>Rayanne</Name>
+        <Name>{user.profile.name}</Name>
       </Greeting>
       <TouchableOpacity>
         <Power size={32} color={theme.COLORS.GRAY_400} />
