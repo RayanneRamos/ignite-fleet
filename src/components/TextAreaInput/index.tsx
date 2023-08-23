ba967bf5c3956106d69baa1ba2c8ede83ad1d512
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-23 15:22:49 -0300

    input refs

--- src/components/TextAreaInput/index.tsx
+++ src/components/TextAreaInput/index.tsx
@@ -1,23 +1,29 @@
-import { TextInputProps } from "react-native";
+import { TextInput, TextInputProps } from "react-native";
 import { Container, Input, Label } from "./styles";
 import { useTheme } from "styled-components/native";
+import { forwardRef } from "react";
 
 type Props = TextInputProps & {
   label: string;
 };
 
-export function TextAreaInput({ label, ...rest }: Props) {
-  const theme = useTheme();
+const TextAreaInput = forwardRef<TextInput, Props>(
+  ({ label, ...rest }, ref) => {
+    const theme = useTheme();
 
-  return (
-    <Container>
-      <Label>{label}</Label>
-      <Input
-        placeholderTextColor={theme.COLORS.GRAY_400}
-        multiline
-        autoCapitalize="setences"
-        {...rest}
-      />
-    </Container>
-  );
-}
+    return (
+      <Container>
+        <Label>{label}</Label>
+        <Input
+          ref={ref}
+          placeholderTextColor={theme.COLORS.GRAY_400}
+          multiline
+          autoCapitalize="setences"
+          {...rest}
+        />
+      </Container>
+    );
+  }
+);
+
+export { TextAreaInput };
--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,20 +1,37 @@
+import { useRef } from "react";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { LicensePlateInput } from "../../components/LicensePlateInput";
 import { TextAreaInput } from "../../components/TextAreaInput";
 import { Container, Content } from "./styles";
+import { TextInput } from "react-native";
 
 export function Departure() {
+  const descriptionRef = useRef<TextInput>(null);
+
+  function handleDepartureRegister() {
+    console.log("Ok");
+  }
+
   return (
     <Container>
       <Header title="Saída" />
       <Content>
-        <LicensePlateInput label="Placa do veículo" placeholder="BRA1234" />
+        <LicensePlateInput
+          label="Placa do veículo"
+          placeholder="BRA1234"
+          onSubmitEditing={() => descriptionRef.current?.focus()}
+          returnKeyType="next"
+        />
         <TextAreaInput
+          ref={descriptionRef}
           label="Finalidade"
           placeholder="Vou utilizar o veículo para..."
+          onSubmitEditing={handleDepartureRegister}
+          returnKeyType="send"
+          blurOnSubmit
         />
-        <Button title="Registrar Saída" />
+        <Button title="Registrar Saída" onPress={handleDepartureRegister} />
       </Content>
     </Container>
   );
