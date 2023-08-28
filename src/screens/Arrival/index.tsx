cb41982f25afef3b7bdba4a0baef9b0c0891972a
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-28 16:28:53 -0300

    revisando o app

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -78,13 +78,13 @@ export function Arrival() {
         <LicensePlate>{historic?.license_plate}</LicensePlate>
         <Label>Finalidade</Label>
         <Description>{historic?.description}</Description>
-        {historic?.status === "departure" && (
-          <Footer>
-            <ButtonIcon icon={X} onPress={handleRemoveVehicleUsage} />
-            <Button title="Registrar Chegada" onPress={handleArrivalRegister} />
-          </Footer>
-        )}
       </Content>
+      {historic?.status === "departure" && (
+        <Footer>
+          <ButtonIcon icon={X} onPress={handleRemoveVehicleUsage} />
+          <Button title="Registrar Chegada" onPress={handleArrivalRegister} />
+        </Footer>
+      )}
     </Container>
   );
 }
