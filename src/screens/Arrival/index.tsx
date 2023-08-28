e2cef52c604e83f432babe8c80b0d712e866e37b
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-28 14:36:46 -0300

    criando a tela para registrar chegada

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,5 +1,14 @@
 import { useRoute } from "@react-navigation/native";
-import { Container } from "./styles";
+import {
+  Container,
+  Content,
+  Description,
+  Footer,
+  Label,
+  LicensePlate,
+} from "./styles";
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
 
 type RouteParamsProps = {
   id: string;
@@ -9,5 +18,23 @@ export function Arrival() {
   const route = useRoute();
   const { id } = route.params as RouteParamsProps;
 
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header title="Chegada" />
+      <Content>
+        <Label>Placa do veículo</Label>
+        <LicensePlate>XXX0000</LicensePlate>
+        <Label>Finalidade</Label>
+        <Description>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut cumque
+          aspernatur et. Quo, dolorem alias quibusdam autem tempore architecto
+          placeat suscipit nobis consequuntur enim quam, sit totam sequi magnam
+          dolorum.
+        </Description>
+        <Footer>
+          <Button title="Registrar Chegada" />
+        </Footer>
+      </Content>
+    </Container>
+  );
 }
