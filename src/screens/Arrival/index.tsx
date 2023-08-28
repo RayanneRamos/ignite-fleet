010d73da7bdacc345d2416172440a849d4f40a52
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-28 14:43:41 -0300

    criando o componnete ButtonIcon e a sua estilização

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -9,6 +9,8 @@ import {
 } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import { ButtonIcon } from "../../components/ButtonIcon";
+import { X } from "phosphor-react-native";
 
 type RouteParamsProps = {
   id: string;
@@ -32,6 +34,7 @@ export function Arrival() {
           dolorum.
         </Description>
         <Footer>
+          <ButtonIcon icon={X} />
           <Button title="Registrar Chegada" />
         </Footer>
       </Content>
