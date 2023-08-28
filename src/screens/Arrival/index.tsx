fb440da55dde87cf0707e3c191e0296d5452512c
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-28 15:11:47 -0300

    registrando a chegada

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -48,6 +48,27 @@ export function Arrival() {
     goBack();
   }
 
+  function handleArrivalRegister() {
+    try {
+      if (!historic) {
+        return Alert.alert(
+          "Error",
+          "Não foi possível obter os dados para registrar a chegada do veículo."
+        );
+      }
+
+      realm.write(() => {
+        (historic.status = "arrival"), (historic.updated_at = new Date());
+      });
+
+      Alert.alert("Chegada", "Chegada registrada com sucesso!");
+      goBack();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Error", "Não foi possível registrar a chegada do veículo.");
+    }
+  }
+
   return (
     <Container>
       <Header title="Chegada" />
@@ -58,7 +79,7 @@ export function Arrival() {
         <Description>{historic?.description}</Description>
         <Footer>
           <ButtonIcon icon={X} onPress={handleRemoveVehicleUsage} />
-          <Button title="Registrar Chegada" />
+          <Button title="Registrar Chegada" onPress={handleArrivalRegister} />
         </Footer>
       </Content>
     </Container>
