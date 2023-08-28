73376d2a4a7a0d19d38c3ae6cd22b0887198301e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-28 14:49:48 -0300

    carregando os detalhes do veículo

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -11,6 +11,9 @@ import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { ButtonIcon } from "../../components/ButtonIcon";
 import { X } from "phosphor-react-native";
+import { useObject } from "../../libs/realm";
+import { Historic } from "../../libs/realm/schemas/Historic";
+import { BSON } from "realm";
 
 type RouteParamsProps = {
   id: string;
@@ -19,20 +22,16 @@ type RouteParamsProps = {
 export function Arrival() {
   const route = useRoute();
   const { id } = route.params as RouteParamsProps;
+  const historic = useObject(Historic, new BSON.UUID(id) as unknown as string);
 
   return (
     <Container>
       <Header title="Chegada" />
       <Content>
         <Label>Placa do veículo</Label>
-        <LicensePlate>XXX0000</LicensePlate>
+        <LicensePlate>{historic?.license_plate}</LicensePlate>
         <Label>Finalidade</Label>
-        <Description>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut cumque
-          aspernatur et. Quo, dolorem alias quibusdam autem tempore architecto
-          placeat suscipit nobis consequuntur enim quam, sit totam sequi magnam
-          dolorum.
-        </Description>
+        <Description>{historic?.description}</Description>
         <Footer>
           <ButtonIcon icon={X} />
           <Button title="Registrar Chegada" />
