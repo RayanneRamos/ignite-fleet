29518b93135c243105f051ef443a95d8234932f5
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-29 15:01:50 -0300

    react-native-toast-message

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -14,6 +14,7 @@ import {
   getLastAsyncTimestamp,
   saveLastSyncTimestamp,
 } from "../../libs/asyncStorage/syncStorage";
+import Toast from "react-native-toast-message";
 
 export function Home() {
   const { navigate } = useNavigation();
@@ -84,6 +85,11 @@ export function Home() {
     if (percentage === 100) {
       await saveLastSyncTimestamp();
       fetchHistoric();
+
+      Toast.show({
+        type: "info",
+        text1: "Todos os dados estão sincronizados.",
+      });
     }
   }
 
