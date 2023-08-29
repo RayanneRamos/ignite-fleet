ddd46d3086973bd4b8c409656882628f9d39b7f0
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-29 14:26:00 -0300

    criando o componente TopMessage e a sua estilização

--- App.tsx
+++ App.tsx
@@ -16,6 +16,8 @@ import { Home } from "./src/screens/Home";
 import { Routes } from "./src/routes";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { RealmProvider, syncConfig } from "./src/libs/realm";
+import { TopMessage } from "./src/components/TopMessage";
+import { WifiSlash } from "phosphor-react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -35,6 +37,7 @@ export default function App() {
             backgroundColor="transparent"
             translucent
           />
+          <TopMessage title="Você está off-line." icon={WifiSlash} />
           <UserProvider fallback={SignIn}>
             <RealmProvider sync={syncConfig} fallback={Loading}>
               <Routes />
